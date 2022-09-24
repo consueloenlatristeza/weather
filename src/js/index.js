01ccd99adc7053b3622b469d2945a93cdf437c4d
Mary Zzz <110771412+consueloenlatristeza@users.noreply.github.com>
2022-09-24 21:03:02 +0200

    add temp

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,20 @@
 
 addEventListener("load", function() {
+    let time = new Date();
+    var currentTime = date.getHours() + ":" + date.getMinutes();
+	document.getElementById("time").innerHTML = currentTime;
+
+    let temp = 22;
+
+    document.getElementById("celcius").addEventListener("click", function(e) {
+        document.getElementById("temp").innerText = temp;
+    });
+
+    document.getElementById("fahrenheit").addEventListener("click", function(e) {
+        document.getElementById("temp").innerText = temp * 1.8 + 32;
+    });
+
+
     const searchEl = document.getElementById("search");
     searchEl.addEventListener("keydown", function(e) {
         if(e.key == "Enter") {
