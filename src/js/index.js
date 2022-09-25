020a71a5e9c8d95894169f8fa34afb219e1a352e
Mary Zzz <110771412+consueloenlatristeza@users.noreply.github.com>
2022-09-25 13:21:14 +0200

    ajax

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
 
 addEventListener("load", async function() {
 
+    const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=35.6785&longitude=139.6823&hourly=temperature_2m");
+    const weatherData = await response.json();
+    console.log(weatherData);
 
     let date = new Date();
     var currentTime = date.getHours() + ":" + date.getMinutes();
