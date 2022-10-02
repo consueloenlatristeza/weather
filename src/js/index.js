212a17dda4d569eede77af5a15de5e87766ded37
Mary Zzz <110771412+consueloenlatristeza@users.noreply.github.com>
2022-10-02 19:08:17 +0200

    add get weather function

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,26 @@
 
-addEventListener("load", async function() {
-
-    const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=35.6785&longitude=139.6823&hourly=temperature_2m");
+function getWeather(lat, long) {
+    const link = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m`;
+    const response = await fetch(link);
     const weatherData = await response.json();
-    console.log(weatherData);
+
+    const date = new Date();
+    temperature = weatherData.hourly["temperature_2m"][date.getHours()];
+    document.getElementById("temp").innerText = temperature;
+}
+
+addEventListener("load", async function() {
+    let temperature = 22;
+
+    navigator.geolocation.getCurrentPosition(async function(position) {
+        const lat = position.coords.latitude;
+        const long = position.coords.longitude;
+        getWeather(lat, long);
+    });
 
     let date = new Date();
     var currentTime = date.getHours() + ":" + date.getMinutes();
 	document.getElementById("time").innerHTML = currentTime;
-
-    let temperature = 22;
 
     document.getElementById("celsius").addEventListener("click", function(e) {
         document.getElementById("temp").innerText = temperature;
@@ -21,10 +32,17 @@ addEventListener("load", async function() {
 
 
     const searchEl = document.getElementById("search");
-    searchEl.addEventListener("keydown", function(e) {
+    searchEl.addEventListener("keydown", async function(e) {
         if(e.key == "Enter") {
             const city = searchEl.value;
-            document.getElementById("city").innerHTML = city;
+            const response = await fetch(`https://geocode.xyz/${city}?json=1`)
+            const cityData = await response.json();
+            if(cityData.error != null) {
+                alert("The Place cannot found!");
+            }else {
+                getWeather(cityData.latt, cityData.longt);
+                document.getElementById("city").innerHTML = `${cityData.standard.city}, ${cityData.standard.countryname}`;
+            }
             searchEl.value = "";
         }
     });
